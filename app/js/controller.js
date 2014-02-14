b14679c59fdf2d3d68d039717fdd7d571d048ebd
shunter1112 <shunter1112@gmail.com>
2014-02-14 07:53:58 +0100

    Implemented addNote function

--- app/js/controller.js
+++ app/js/controller.js
@@ -2,15 +2,91 @@ var naturalLog = angular.module('naturalLog', ['angularLocalStorage','ngCookies'
 
 naturalLog.controller('LogCtrl',['$scope','storage',function($scope, storage) {
     
+  var default_uuid = "8982a835-bc35-edb3-4252-646f622e0d25";
+  /*
+  [
+  { 
+    uuid : ""
+    note_title : "Title of note",
+    date_notes:[
+      {
+        date: "When / YYYY/MM/DD"
+        notes : [
+          {
+            note: "Content",
+            time: "When it was / HH:mm:ss"
+          },...
+        ]
+      },...
+    ]
+  },...
+  ] 
+  */
+    
   storage.bind($scope, 'logs', {defaultValue: [
-    {note:'======ウォークショップの開始========', time:"12:33:20"},
-    {note:'彼が喋り出した', time:"12:33:44"}
-  ]}); 
+    {
+      uuid : default_uuid,
+      note_title : "広島大学第１回WS",
+      date_notes : [
+        {
+          date: "2014/02/10",
+          notes : [
+            {time: "09:00:12", content: "ワークショップ開始"},
+            {time: "09:00:12", content: "どうしたものかなぁ"},
+            {time: "09:00:12", content: "困ったね"},
+            {time: "09:12:90", content: "ワークショップで焦る人続出"}
+          ]
+        }
+      ]
+    },
+    {
+      uuid : String(guid()),
+      note_title : "九州大学第１回WS",
+      date_notes : [
+        {
+          date: "2014/02/14",
+          notes : [
+            {time: "09:00:12", content: "はあ"},
+            {time: "09:00:12", content: "どうのかなぁ"},
+            {time: "09:00:12", content: "たね"},
+            {time: "09:12:90", content: "ョップで焦る人続出"}
+          ]
+        },
+        {
+          date: "2014/02/12",
+          notes : [
+            {time: "09:00:12", content: "テス"},
+            {time: "09:00:12", content: "どぁ"},
+            {time: "09:00:12", content: "たね"},
+            {time: "09:12:90", content: "ョッ続出"}
+          ]
+        }
+      ]
+    }
+  ]
+  }); 
+  storage.bind($scope, 'noteFilter', {defaultValue: default_uuid} );
  
-  $scope.addTodo = function() {
-    console.log();
-    $scope.logs.push({note:$scope.logNote, time:getCurrentTime()});
-    $scope.logNote = '';
+  $scope.addNote = function() {
+
+    var thatLog = $scope.logs.filter(function(item, index){
+      if (item.uuid == $scope.noteFilter) return true;
+    });
+
+    var thatDateNote 
+    while(!thatDateNote){
+      thatDateNote = thatLog[0].date_notes.filter(function(item, index){
+        if (item.date == getCurrentDate()) return true;
+      });
+      if(thatDateNote.length==0) {
+        thatDateNote = null;
+        thatLog[0].date_notes.push({date: String(getCurrentDate()), notes: []})
+      }
+    }
+
+    thatDateNote[0].notes.push({ content:$scope.noteContent, time:getCurrentTime()});
+
+    $scope.noteContent = '';
     setTimeout(function(){
       
       var insideWindowTable = document.querySelector(".logbody table");
@@ -20,7 +96,8 @@ naturalLog.controller('LogCtrl',['$scope','storage',function($scope, storage) {
       // console.log(insideWindow.scrollTop);
     
     },50);
-
   };
+  
+  $scope.resetLog = function(){ storage.clearAll(); }
   
 }]);
